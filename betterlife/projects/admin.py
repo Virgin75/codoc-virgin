8dbf25c34acee4b44e088e7ea4b3aff2de03ca41
Virgin Bitton <virgin.bitton@gmail.com>
2022-06-22 19:04:46 +0200

    register the Comment model in Admin

--- betterlife/projects/admin.py
+++ betterlife/projects/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Project, ProjectGoal, ProjectMember
+from .models import Project, ProjectGoal, ProjectMember, Comment
 
 admin.site.register(Project)
 admin.site.register(ProjectGoal)
 admin.site.register(ProjectMember)
+admin.site.register(Comment)
